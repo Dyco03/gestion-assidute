18268bbafcb79d6a3d4051b214b991cbe4aede0f
Dyco03 <miasadyco@gmail.com>
2025-06-05 00:27:46 +0300

    création du model

---
+++ database/migrations/2025_06_04_190003_create_etudiants_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('etudiants', function (Blueprint $table) {
+            $table->id("id_etudiant");
+            $table->unsignedBigInteger("id_personne");
+            $table->unsignedBigInteger("id_niveau");
+            $table->string("info");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('etudiants');
+    }
+};
